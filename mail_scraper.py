2b6ca2d18130270689483e516888fcb195318756
PonykillerMX <emilioanderevalencia@gmail.com>
2021-02-11 16:07:17 -0500

    added a function that has no function in life

--- mail_scraper.py
+++ mail_scraper.py
@@ -1,8 +1,14 @@
 from link_scraper import LinkScraper
 
 # create a class to scrape the links 
-fortnite = LinkScraper(10, "fortnite")     
-print(fornite.search())
+# this will return a list of 10 links
+fortnite_links = LinkScraper(10, "fortnite")     
+
+def find_user_email(fornite_links): 
+    # find the user that posted the link's video email. 
+    for link in fornite_links: 
+         
+
 
 
 
